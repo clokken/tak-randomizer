cfd8ad7aa796a5b661b28103b4c9d6022f0f22fa
C L <c1000@protonmail.com>
2021-08-15 16:15:46 -0300

    Fixed deployment

--- server/server.ts
+++ server/server.ts
@@ -1,23 +1,37 @@
 import * as Http from 'http';
 import * as SocketIo from 'socket.io';
+import express from "express";
+import * as Path from 'path';
 import { MainServer } from './main';
 
 const serverPort = process.env['SERVER_PORT'] || '3000';
 
 const start = async () => {
-    const http = Http.createServer();
+    const app = express();
+    const http = Http.createServer(app);
+
     const io = new SocketIo.Server(http, {
-        cors: {
+        /*cors: {
             origin: "http://localhost:3000", // TODO
             methods: ["GET", "POST"]
-        },
+        },*/
     });
 
     const server = new MainServer(io);
 
     await server.start();
-    io.listen(parseInt(serverPort));
-    console.log(`Server listening at port ${serverPort}`);
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(Path.join(__dirname, '../build')));
+
+        app.get('*', function(req, res) {
+          res.sendFile(Path.join(__dirname, '../build', 'index.html'));
+        });
+    }
+
+    http.listen(parseInt(serverPort), () => {
+        console.log(`Server listening at port ${serverPort}`);
+    });
 };
 
 start()
